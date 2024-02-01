79a136b355a5315ace952b74c9bf7354e1f866bd
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-01 14:12:22 -0300

    model usuarios

--- DeudasArs/Usuarios/models.py
+++ DeudasArs/Usuarios/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+
 class Usuario(models.Model):
-    
+    email = models.EmailField()
+    password = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
