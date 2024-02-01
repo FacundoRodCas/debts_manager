4b9332d2437140e05b7ee37a39f27dd0bd8f2b72
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-01 13:19:19 -0300

    mostramos dolar blue en Home

--- DeudasArs/DeudasArs/settings.py
+++ DeudasArs/DeudasArs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Deudores',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'DeudasArs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/Facundo/Desktop/Deudas/DeudasArs/DeudasArs/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DeudasArs/DeudasArs/views.py
+++ DeudasArs/DeudasArs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from Deudores.dolarjson import dolar_blue
 
-def mostrar(request):
-    pass
+def dolarblue(request):
+    dolar = dolar_blue
+    return render(request, "home.html", {"dolar": dolar})
