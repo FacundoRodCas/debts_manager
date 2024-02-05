018121ed5ca376d6c9040411b1309e13ba6073db
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-05 18:46:05 -0300

    Mostrar deudores

--- DeudasArs/Deudores/views.py
+++ DeudasArs/Deudores/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Deudores
+from .forms import FormuarioDeudores
 
 def mostrar_deudores(request):
-    return render(request, "deudores.html")
+    deudores = Deudores.objects.filter()
+    return render(request, "deudores.html", {'deudores': deudores})
