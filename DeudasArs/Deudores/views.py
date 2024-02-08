246ecf99521693c5cfe91904cbe84afcffc01325
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-08 14:11:26 -0300

    Modificamos la view de modificar_deuda para que tenga en cuenta el valor json

--- DeudasArs/Deudores/views.py
+++ DeudasArs/Deudores/views.py
@@ -3,6 +3,7 @@ from .models import Deudores
 from .forms import FormularioDeudores
 from .dolarjson import dolar_blue
 
+
 def mostrar_deudores(request):
     if request.method == 'GET':
         deudores = Deudores.objects.filter()
@@ -12,6 +13,7 @@ def mostrar_deudores(request):
             deudas_actualizadas.append(deuda_actualizada)
         deudas = zip(deudores, deudas_actualizadas)
         return render(request, "deudores.html", {'deudas': deudas})
+
 
 def crear_deuda(request):
     if request.method == 'POST':
@@ -30,16 +32,20 @@ def crear_deuda(request):
         form = FormularioDeudores()
         return render(request, "crearusuario.html", {'form': form})
 
+
 def modificar_deuda(request, pk):
     deuda = Deudores.objects.get(pk=pk)
     if request.method == 'POST':
         form = FormularioDeudores(request.POST, instance=deuda)
         if form.is_valid():
+            deudor = form.save(commit=False)
+            deudor.deuda_inicial_dolares = deudor.deuda_inicial_pesos / dolar_blue
             form.save()
             return redirect('deudores:')
     else:
         form = FormularioDeudores(instance=deuda)
     return render(request, "modificar.html", {'form': form})
+
 
 def eliminar_deuda(request, pk):
     deuda = Deudores.objects.get(pk=pk)
