97e355455ef2bfe500d31448f700f1e1f63faccc
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-17 16:51:26 -0300

    commit

--- DeudasArs/Deudores/urls.py
+++ DeudasArs/Deudores/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import mostrar_deudores, crear_deuda, modificar_deuda, eliminar_deuda
+
 app_name = "deudores"
 urlpatterns = [
     path('deudores/', mostrar_deudores, name=""),
