d9731a6304ce105b7e8acb54d248d857016042c4
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-17 16:33:42 -0300

    View del login

--- DeudasArs/Cuenta/views.py
+++ DeudasArs/Cuenta/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            usuario = authenticate(rquest,
+                                   usuario=cd['usuario'],
+                                   password=cd['password'])
+            if usuario is not None:
+                if usuario.is_active:
+                    login(request, usuario)
+                    return render(request, 'usuario autenticado')
+                else:
+                    return render(request, 'usuario inactivo')
+            else:
+                return render(request, "información incorrecta")
+        else:
+            return render(request, "home.html", {'form': form})
