730b819ccef08d17ab5742ec88eb903af2b4dedf
Facundo <fa,rodriguez.castro@gmail.com>
2024-02-22 16:41:55 -0300

    problema de importacion de modulo solucionado

--- DeudasArs/DeudasArs/views.py
+++ DeudasArs/DeudasArs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from DeudasArs.Deudores.dolarjson import dolar_blue
+from Deudores.dolarjson import dolar_blue
 
 def dolarblue(request):
     dolar = dolar_blue
